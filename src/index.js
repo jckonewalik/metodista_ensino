3003d52188d826624dcfa22f92c645d63c280d74
João Carlos Konewalik de Souza <jckonewalik@gmail.com>
2020-01-09 10:40:52 -0300

    redirect http to https

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import HttpsRedirect from 'react-https-redirect';
 import ReactDOM from 'react-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 import App from './app/App';
 import { store, persistor } from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
+const env = runtimeEnv();
+
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate persistor={persistor}>
-      <App />
+      <HttpsRedirect disabled={env.NODE_ENV === 'development'}>
+        <App />
+      </HttpsRedirect>
     </PersistGate>
   </Provider>,
   document.getElementById('root'),
